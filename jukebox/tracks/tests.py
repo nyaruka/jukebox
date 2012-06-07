7f5a22ff006b53837c6faea129bee759c5bc1040
Nic Pottier <nicpottier@gmail.com>
2012-06-07 10:05:53 +0200

    2nd week of live coding, start adding a way of adding tracks and inferring attributes from mp3 files

--- jukebox/tracks/tests.py
+++ jukebox/tracks/tests.py
@@ -1,16 +1,17 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from .models import *
+from django.contrib.auth.models import User
 
+class TrackTest(TestCase):
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_importing(self):
+        user = User.objects.create_user('admin', 'admin@admin.com', 'admin')
+
+        track = Track.create_from_file("../womanizer.mp3", user)
+
+        self.assertEquals(track.name, "Womanizer")
+        self.assertEquals(track.album.name, "Circus")
+        self.assertEquals(track.album.artist.name, "Britney Spears")
+        self.assertEquals(track.genre.name, "Pop")
+        
+        
--- jukebox/tracks/views.py
+++ jukebox/tracks/views.py
@@ -9,32 +9,46 @@ class ArtistCRUDL(SmartCRUDL):
         fields = ('name', 'created_on')
         search_fields = ('name__icontains',)
 
+
 class TrackCRUDL(SmartCRUDL):
     model = Track
     actions = ('create', 'list', 'read', 'update', 'delete')
 
+    class Create(SmartCreateView):
+        fields = ('mp3_file',)
+
+        def pre_save(self, obj):
+            obj = super(TrackCRUDL.Create, self).pre_save(obj)
+            return Track.create_from_file(obj.mp3_file.file.temporary_file_path(), obj.created_by)
+
     class List(SmartListView):
-        fields = ('title', 'artist', 'length', 'genre', 'album')
-        search_fields = ('title__icontains', 'album__artist__name__icontains')
+        fields = ('name', 'artist', 'length', 'genre', 'album')
+        search_fields = ('name__icontains', 'album__artist__name__icontains')
 
         def get_artist(self, obj):
-            return obj.album.artist.name
+            if obj.album:
+                return obj.album.artist.name
+            else:
+                return ""
 
         def get_length(self, obj):
-            mins = obj.length / 60
-            secs = obj.length % 60
+            if obj.length:
+                mins = obj.length / 60
+                secs = obj.length % 60
 
-            if mins <= 0:
-                return "%ds" % secs
+                if mins <= 0:
+                    return "%ds" % secs
+                else:
+                    return "%dm %ds" % (mins, secs)
             else:
-                return "%dm %ds" % (mins, secs)
+                return ""
 
 class AlbumCRUDL(SmartCRUDL):
     model = Album
     actions = ('create', 'list', 'read', 'update')
 
     class List(SmartListView):
-        fields = ('title', 'artist', 'year')
+        fields = ('name', 'artist', 'year')
 
 
 class GenreCRUDL(SmartCRUDL):
