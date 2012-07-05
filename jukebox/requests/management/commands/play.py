fff50a31990f0fd17b93696e2711e4ccad208d60
norkans7 <norkans7@gmail.com>
2012-07-05 13:17:00 +0200

    used request.create to get the random track to play when no request is queued

--- jukebox/requests/management/commands/play.py
+++ jukebox/requests/management/commands/play.py
@@ -7,6 +7,7 @@ from subprocess import call
 import sys
 import os
 from time import sleep
+from django.contrib.auth.models import User
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list
@@ -14,6 +15,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         tracklib = Track.objects.all()
         lock_file = '/tmp/player.lock'
+        user = User.get_anonymous()
         if os.path.exists(lock_file):
             sys.exit(0)
         else:
@@ -40,19 +42,15 @@ class Command(BaseCommand):
                         finally:
                             request.status = 'C'
                             request.save()
-
-                    while(not playlist):
+                    
+                    if not playlist:
                         randomlist = tracklib.order_by('?')
-                        for nyimbo in randomlist:
-                            try:
-                                call(["mpg123", nyimbo.mp3_file.path])
-                                playlist = Request.objects.filter(status='Q').order_by('created_on')
-                                if playlist:
-                                     break
-                            except:
-                                import traceback
-                                traceback.print_exc()
-                                sys.exit(1)
+                        Request.objects.create(track=randomlist[0],
+                                           created_by=user,
+                                           modified_by=user,
+                                           played_on =datetime.datetime.now())
+                        
+                        
                        
                                
 
