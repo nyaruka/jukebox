b4156cc80d0c5b77120e101d85847671b0f9a39d
Norbert KWIZERA <norkans7@gmail.com>
2012-09-12 11:49:31 +0200

    changed order of tracks list

--- jukebox/tracks/views.py
+++ jukebox/tracks/views.py
@@ -35,6 +35,7 @@ class TrackCRUDL(SmartCRUDL):
     class List(SmartListView):
         fields = ('name', 'artist', 'length', 'genre', 'album', 'request')
         search_fields = ('name__icontains', 'album__artist__name__icontains')
+        default_order = ('-created_on',)
 
         def derive_queryset(self, **kwargs):
             queryset = super(TrackCRUDL.List, self).derive_queryset(**kwargs)
