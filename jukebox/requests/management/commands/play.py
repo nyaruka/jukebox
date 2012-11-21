bff55496754f2fd0bd4ca8b89c8ed6114992e651
jukebox <jukebox@kLab.(none)>
2012-11-21 17:30:18 +0200

    Update play command

--- jukebox/requests/management/commands/play.py
+++ jukebox/requests/management/commands/play.py
@@ -31,10 +31,8 @@ class Command(BaseCommand):
                         request.played_on = datetime.datetime.now()
                         request.save()
 
-                        try:
-                            
+                        try:                            
                             call(["mpg123", request.track.mp3_file.path])
-                            
 			except:
                             import traceback
                             traceback.print_exc()
@@ -45,31 +43,30 @@ class Command(BaseCommand):
                     
                     if not playlist:
 
-                        #generate a random track request while no more request to play   
-                        # select t.name, count(*) as 'requests', track_id from requests_request, tracks_track t where t.id = requests_request.track_id and requests_request.created_by_id != -1 group by track_id order by requests;
-
                         # find all songs that have been requested by real users
                         requests = Request.objects.exclude(created_by_id=-1)
 
                         # now exclude any song that has ever been voted down
-                        requests = requests.exclude(track_id__in=[t.id for t in Vote.objects.filter(score=-1)])
+                        requests = requests.exclude(track_id__in=[t.track_id for t in Vote.objects.filter(score=-1)])
 
                         # exclude anything that has been played recently
-                        repeat_window = datetime.timedelta(hours=6)
-                        requests = requests.exclude(created_on__gt=datetime.datetime.now() - repeat_window)
+                        window = datetime.datetime.now() - datetime.timedelta(hours=6)
+                        requests = requests.exclude(track_id__in=[t.track_id for t in Request.objects.filter(created_on__gt=window)])
 
-                        randomlist = requests.order_by('?')
-                        if randomlist:
-                            Request.objects.create(track=randomlist[0].track,
+                        self.stdout.write("Considering %d requests before %s" % (requests.count(), window))
+
+                        if requests:
+                            requests = requests.order_by('?')
+                            Request.objects.create(track=requests[0].track,
                                            created_by=user,
                                            modified_by=user,
-                                           played_on =None)
+                                           played_on=None)
                         
-                                                #for the bug of tracks stucking on the playing status because of an unxpected system halt 
+                        #for the bug of tracks stucking on the playing status because of an unxpected system halt 
                         request_completed = Request.objects.filter(status='P').order_by('created_on')
                         if request_completed:
                             for req in request_completed:
-                                if datetime.datetime.now() - req.played_on > req.track.length:
+                                if long((datetime.datetime.now() - req.played_on).total_seconds()) > req.track.length:
                                     req.status = 'C'
                                     req.save()
                        
